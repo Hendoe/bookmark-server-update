8d5f852775f351ca1fb32f536c28607d2501290a
Hendoe <hendo1349@gmail.com>
2020-09-24 10:44:40 -0500

    intial commit

---
+++ seeds/seed.bookmark_table.sql
@@ -0,0 +1,7 @@
+INSERT INTO bookmark_table (title, url, description, rating)
+VALUES
+  ('Book One', 'Interview',
+    'Lor natus consequuntur dipisci autem neque?', 2),
+  ('Book Two', 'Review',
+    'Natus contur dip tem neque?', 4)
+  ;
